b24a8540ff8ab2ee5f27f0d4f29dc69f2a48e826
Ankit Kamboj <er.ankitkamboj@gmail.com>
2014-04-27 14:05:29 +0530

    questions/TODOs for basics,binary and control flow in java

--- src/main/java/com/techarha/training/java/basics/question/Basic3.java
+++ src/main/java/com/techarha/training/java/basics/question/Basic3.java
@@ -1,9 +1,12 @@
 package com.techarha.training.java.basics.question;
 
 /**
- * @question Create a class containing an uninitialized String reference. Demonstrate that this reference is initialized by Java to null.
- * @explaination NA
+ * @question1 Create a class containing an uninitialized String reference. Demonstrate that this reference is initialized by Java to null.
+ * @explaination1 NA
  *
+ * @question2 Create a main( ) that uses varargs instead of the ordinary main( ) syntax. Print all the elements in the
+ *           resulting args array. Test it with various numbers of command-line arguments.
+ * @explaination2 NA
  * @author ankit.
  */
 public class Basic3 {
---
+++ src/main/java/com/techarha/training/java/enums/enums.java
@@ -0,0 +1,17 @@
+package com.techarha.training.java.enums;
+
+/**
+ * @question1 Create an enum of the least-valuable six types of paper currency. Loop through the values( ) and print
+ *           each value and its ordinal( ).
+ * @explaination1 declare enum inside class and then call values() function to loop through its values.
+ *
+ * @question2 Write a switch statement for the enum in the previous example. For each case, output a description of that
+ *           particular currency.
+ * @explaination2 pass enum variable created in switch and use possible enum constants as case constants.
+ *
+ * @author ankit.
+ */
+public class enums {
+
+    //TODO complete above queries
+}
